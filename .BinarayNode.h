28948825fe36222a03e39ad2e54285b0decd4cfa
tdavies4 <tdavies4@users.noreply.github.com>
2015-11-16 12:15:46 -0800

    Update and rename .gitattributes to .BinarayNode.h

---
+++ .BinarayNode.h
@@ -0,0 +1,39 @@
+//
+//  BinaryNode.h
+//  group ibrahim part
+//
+//  Created by ibrahim ibrahim on 6/9/15.
+//  Copyright (c) 2015 ibrahim ibrahim. All rights reserved.
+//
+
+
+
+template<class ItemType>
+class BinaryNode
+{
+private:
+    ItemType  item;
+    
+    
+    // Data portion
+    BinaryNode<ItemType>* leftPtr;		// Pointer to left child
+    BinaryNode<ItemType>* rightPtr;		// Pointer to right child
+    
+public:
+    // constructors
+    BinaryNode(const ItemType & anItem)	{item = anItem; leftPtr = 0; rightPtr = 0;}
+    BinaryNode(const ItemType & anItem,
+               BinaryNode<ItemType>* left,
+               BinaryNode<ItemType>* right) {item = anItem; leftPtr = left; rightPtr = right;}
+    // accessors
+    void setItem(const ItemType & anItem) {item = anItem;}
+    void setLeftPtr(BinaryNode<ItemType>* left) {leftPtr = left;}
+    void setRightPtr(BinaryNode<ItemType>* right) {rightPtr = right;}
+    // mutators
+    ItemType getItem() const	 {return item;}
+    BinaryNode<ItemType>* getLeftPtr() const  {return leftPtr;}
+    BinaryNode<ItemType>* getRightPtr() const {return rightPtr;}
+    
+    bool isLeaf() const {return (leftPtr == 0 && rightPtr == 0);}
+}; 
+
